92cebcfd97f4709f85b1fe9885862e3f86684c19
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-03 10:58:07 -0300

    home e splash

---
+++ src/atomic/molecules/MainBanner.tsx
@@ -0,0 +1,19 @@
+import { Box, Image } from "native-base";
+import { ImageBackground } from "react-native";
+
+import slider from '../../img/slider.png'
+import bike from '../../img/bike.png'
+
+export function MainBanner() {
+    return(
+        <Box w="100%" h="300" mt="60px">
+            <ImageBackground
+                source={slider}
+                style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+                resizeMode="contain"
+            >
+                <Image source={bike} alt="image of bike" />
+            </ImageBackground>
+        </Box>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Box } from "native-base";
 import { DatailBackground } from "../atomic/atoms/DetailBackground";
+import { MainBanner } from "../atomic/molecules/MainBanner";
 
 export function Home() {
     return(
         <Box flex="1" padding="20px" pt="-10px" position="relative">
             < DatailBackground />
+            <MainBanner />
         </Box>
-
+ 
     )
 }
